92cae8e253bd55d86a4a928f12ca0ac3ba0a5c87
aaaddress1 <aaaddress1@gmail.com>
2015-04-20 21:16:05 +0800

    no message

---
+++ WebBrowserHTTPRequestHook/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WebBrowserHTTPRequestHook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.listBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel1.SuspendLayout();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            this.splitContainer.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer.Panel1
+            // 
+            this.splitContainer.Panel1.Controls.Add(this.webBrowser1);
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.listBox);
+            this.splitContainer.Size = new System.Drawing.Size(666, 308);
+            this.splitContainer.SplitterDistance = 210;
+            this.splitContainer.SplitterWidth = 10;
+            this.splitContainer.TabIndex = 4;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(666, 210);
+            this.webBrowser1.TabIndex = 3;
+            this.webBrowser1.Url = new System.Uri("http://www.google.com", System.UriKind.Absolute);
+            // 
+            // listBox
+            // 
+            this.listBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 12;
+            this.listBox.Location = new System.Drawing.Point(0, 0);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(666, 88);
+            this.listBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 308);
+            this.Controls.Add(this.splitContainer);
+            this.Name = "Form1";
+            this.Text = "WebBrowser HTTP Request Hook , By.aaaddress1.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer.Panel1.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ListBox listBox;
+
+
+    }
+}
+
---
+++ WebBrowserHTTPRequestHook/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+//NEED TO IMPORTANT WINDOWS\SYSTEM32\SHDOCVW.DLL OR IMPORTANT INTERNET EXPLORER CONTROL COM.
+using SHDocVw;
+
+namespace WebBrowserHTTPRequestHook
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SHDocVw.WebBrowser wb = (SHDocVw.WebBrowser)webBrowser1.ActiveXInstance;
+            wb.BeforeNavigate2 += new DWebBrowserEvents2_BeforeNavigate2EventHandler(
+                (object pDisp, 
+                 ref object URL, 
+                 ref object Flags, 
+                 ref object TargetFrameName, 
+                 ref object PostData, 
+                 ref object Headers, 
+                 ref bool Cancel) => 
+                 {
+                     
+                     if (PostData == null)
+                     {
+                         this.listBox.Items.Add("[GET] " + URL);
+                         this.listBox.Items.Add("");
+                     }
+                     else
+                     {
+                         string PostDATAStr = System.Text.Encoding.ASCII.GetString((Byte[])PostData);
+                         this.listBox.Items.Add("[POST] " + URL);
+                         this.listBox.Items.Add("[POST DATA] " + PostDATAStr);
+                         this.listBox.Items.Add("");
+                     }
+                 });
+        }
+
+    }
+}
